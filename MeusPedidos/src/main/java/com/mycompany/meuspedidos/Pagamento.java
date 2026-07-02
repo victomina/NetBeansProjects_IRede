9caee6d2c8c333a8682267592920ccff06c76594
victomina <victomina@protonmail.com>
2026-07-01 22:01:20 -0300

    Adicionando métodos na classe

--- MeusPedidos/src/main/java/com/mycompany/meuspedidos/Pagamento.java
+++ MeusPedidos/src/main/java/com/mycompany/meuspedidos/Pagamento.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.meuspedidos;
 
-/**
- *
- * @author Usuário
- */
-public class Pagamento {
+
+public abstract class Pagamento {
+    protected double valor;
     
+    public Pagamento(double valor){
+        this.valor = valor;
+    }
+    
+    //contrato obrigatório
+    
+    //classe abstratas são classes que não são instanciadas
+    public abstract void processar();
+    public abstract  double calcularTaxa();
+    
+    public double valorFinal(){
+        return valor +calcularTaxa();
+    }
 }
