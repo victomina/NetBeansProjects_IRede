7a804b15f14b3691486b46e709be367cadcdd41c
victomina <victomina@protonmail.com>
2026-07-04 14:00:09 -0300

    Adicionando classe 'PagamentoPix'

---
+++ MeusPedidos/src/main/java/com/mycompany/meuspedidos/PagamentoPix.java
@@ -0,0 +1,19 @@
+package com.mycompany.meuspedidos;
+
+public class PagamentoPix extends Pagamento {
+
+    public PagamentoPix(double valor) {
+        super(valor);
+    }
+
+    @Override
+    public void processar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double calcularTaxa() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
