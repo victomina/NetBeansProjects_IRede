a0381898b2d7f5890226d013b38dd88f2cba8750
victomina <victomina@protonmail.com>
2026-07-04 16:57:28 -0300

    Adicionando metono na classe principal

--- MeusPedidos/src/main/java/com/mycompany/meuspedidos/Pedido.java
+++ MeusPedidos/src/main/java/com/mycompany/meuspedidos/Pedido.java
@@ -1,15 +1,34 @@
 package com.mycompany.meuspedidos;
 
+import java.util.List;
+
 public class Pedido {
     
     private Cliente cliente;
+    private List<ItemPedido> itens;
     private double total;
+    
+    public Pedido(Cliente cliente, double total) {
+        this.cliente = cliente;
+        this.total = total;
+    }
 
-//    public Pedido(Cliente cliente, double total) {
-//        this.cliente = cliente;
-//        this.total = total;
-//    }
-
+    public void adicionarItem(Produto produto, int quantidade){
+        ItemPedido itempedido = new ItemPedido(produto,quantidade);
+        itempedido.add(itempedido);
+    }
+    
+    public double calcularTotal(){
+        if(itens.isEmpty()){
+            return 0;
+        }
+       double total = 0;
+       for(ItemPedido item : itens){
+         total = total + item.calcularSubtotal();
+       }
+         return total;
+    }
+    
     public Cliente getCliente() {
         return cliente;
     }
