f1c57af71af37a6f0d989d268c162fddd9c929ee
victomina <victomina@protonmail.com>
2026-07-04 13:43:01 -0300

    Adicionando

--- MeusPedidos/src/main/java/com/mycompany/meuspedidos/PagamentoCartao.java
+++ MeusPedidos/src/main/java/com/mycompany/meuspedidos/PagamentoCartao.java
@@ -1,5 +1,22 @@
 package com.mycompany.meuspedidos;
 
 public class PagamentoCartao extends Pagamento {
+
+    private int parcelas;
+
+    public PagamentoCartao(double valor, int parcelas) {
+        super(valor);
+        this.parcelas = parcelas;
+    }
+        
+    @Override
+    public void processar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double calcularTaxa() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
