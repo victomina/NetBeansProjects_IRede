2bb7c423b5564626f99754e047afdaac3b797477
victomina <victomina@protonmail.com>
2026-07-06 13:29:01 -0300

    Program para testar os erros

--- PlaygroundErros/src/main/java/br/org/irede/poo/playgrounderros/PlaygroundErros.java
+++ PlaygroundErros/src/main/java/br/org/irede/poo/playgrounderros/PlaygroundErros.java
@@ -1,16 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.org.irede.poo.playgrounderros;
 
-/**
- *
- * @author Usuário
- */
+import java.util.Scanner;
+
 public class PlaygroundErros {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("==== Bem vindos ao show de horrores do Java ===== \n");
+        Scanner ler = new Scanner(System.in);
+        
+        exemploArithmeticException(ler);
+        
+        ler.close();
+        System.out.println(" === Sobrevivemos ===");
+    }
+
+    private static void exemploArithmeticException(Scanner ler) {
+        System.out.println("Informe o primeiro numero: ");
+        int n1 = ler.nextInt();
+        System.out.println("Informe o segundo numero: ");
+        int n2 = ler.nextInt();
+        int resultado = n1/n2;
+        System.out.println(" Resultado: " + resultado);
     }
 }
