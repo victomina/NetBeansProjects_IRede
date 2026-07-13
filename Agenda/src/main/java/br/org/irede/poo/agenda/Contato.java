cfc3c32df6d1f45e5c0ee6ba972981ab03d24922
victomina <victomina@protonmail.com>
2026-07-13 02:12:52 -0300

    Adicionando dados da agenda

--- Agenda/src/main/java/br/org/irede/poo/agenda/Contato.java
+++ Agenda/src/main/java/br/org/irede/poo/agenda/Contato.java
@@ -5,5 +5,29 @@ package br.org.irede.poo.agenda;
  * @author Victor Veríssimo
  */
 public class Contato {
+    private String Nome;
+    private String Telefone;
+
+    public Contato(String Nome, String Telefone) {
+        this.Nome = Nome;
+        this.Telefone = Telefone;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+    
     
 }
