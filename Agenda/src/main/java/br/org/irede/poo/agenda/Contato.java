3072125e157061eeb963bbfd560912c6daf2be72
victomina <victomina@protonmail.com>
2026-07-13 02:17:24 -0300

    Adicionando a classe contato

--- Agenda/src/main/java/br/org/irede/poo/agenda/Contato.java
+++ Agenda/src/main/java/br/org/irede/poo/agenda/Contato.java
@@ -5,29 +5,42 @@ package br.org.irede.poo.agenda;
  * @author Victor Veríssimo
  */
 public class Contato {
-    private String Nome;
-    private String Telefone;
+    private String nome;
+    private String telefone;
+    private String identificador;
 
-    public Contato(String Nome, String Telefone) {
-        this.Nome = Nome;
-        this.Telefone = Telefone;
+    public Contato(String nome, String telefone, String identificador) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.identificador = identificador;
     }
 
     public String getNome() {
-        return Nome;
+        return nome;
     }
 
-    public void setNome(String Nome) {
-        this.Nome = Nome;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getTelefone() {
-        return Telefone;
+        return telefone;
     }
 
-    public void setTelefone(String Telefone) {
-        this.Telefone = Telefone;
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
-    
-    
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+   
+    @Override
+    public String toString(){
+        return identificador + " | " + nome + " | " + telefone;
+    }
 }
