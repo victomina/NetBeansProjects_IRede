ac227f324d84783dfea3432522f9e3b168b99ba4
victomina <victomina@protonmail.com>
2026-07-13 02:25:14 -0300

    Consertando algumas coisas

--- Agenda/src/main/java/br/org/irede/poo/agenda/Agenda.java
+++ Agenda/src/main/java/br/org/irede/poo/agenda/Agenda.java
@@ -1,16 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.org.irede.poo.agenda;
+
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Usuário
+ * @author Victor Veríssimo
  */
 public class Agenda {
+    
+    static int contador = 0;
+    static Contato[] contatos = new Contato[3];
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i < contatos.length; i ++){
+            contatos[i] = criarContato();
+        }
+        exibeContatos(contatos);
+    }
+    
+    public static Contato criarContato(){
+       try{
+           Contato c = new Contato();
+           c.setNome(capturaTexto("Informe o nome"));
+           c.setTelefone(capturaTexto("Informe o telefone"));
+           validarContato(c.getTelefone());
+           contador = contador +1;
+           c.setIdentificador(contador);
+           return c;
+       }catch(contatoDuplicadoException e){
+           JOptionPane.showMessageDialog(null, e.getMessage(), ".:Sistema de Agenda:.", JOptionPane.ERROR_MESSAGE );
+       } 
+       return null;
+    }
+    
+    public static void validarContato(String telefone) throws ContatoDuplicadoException{
+        for (Contato contato : contatos){
+            if(contato != null && contato.getTelefone().equals(telefone)){
+                throw new ContatoDuplicadoException(contato);
+            }
+        }
+    }
+    
+    public static String capturaTexto(String texto){
+        return JOptionPane.showInputDialog(null, texto, ".:Sistema Agenda:.", JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    public static void exibeDados(Contato[] contatos){
+        String mensagem = "";
+        for (Contato contato : contato){
+            mensagem = mensagem + contato + "\n";
+        }
+        JOptionPane.showMessageDialog(null, mensagem, "Contato", JOptionPane.INFORMATION_MESSAGE);
     }
 }
--- Agenda/src/main/java/br/org/irede/poo/agenda/ContatoDuplicadoException.java
+++ Agenda/src/main/java/br/org/irede/poo/agenda/ContatoDuplicadoException.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.org.irede.poo.agenda;
 
 /**
  *
- * @author Usuário
+ * @author Victor Veríssimo
  */
-public class ContatoDuplicadoException {
+public class ContatoDuplicadoException extends Exception{   
+    public ContatoDuplicadoException(Contato contato){
+        super("O telefone  " + contato.getTelefone() + "está cadastrado para " + contato.getNome());
+    }
     
 }
