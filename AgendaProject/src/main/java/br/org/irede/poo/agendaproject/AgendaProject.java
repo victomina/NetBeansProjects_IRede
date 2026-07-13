67c18006398f66002ad4b748a4bf6b3cf95b1071
victomina <victomina@protonmail.com>
2026-07-13 01:26:46 -0300

    Adicionando projeto da agenda ao desadio Java

---
+++ AgendaProject/src/main/java/br/org/irede/poo/agendaproject/AgendaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.org.irede.poo.agendaproject;
+
+/**
+ *
+ * @author Usuário
+ */
+public class AgendaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
