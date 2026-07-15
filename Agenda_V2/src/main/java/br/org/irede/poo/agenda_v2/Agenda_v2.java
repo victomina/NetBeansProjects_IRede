1ab768dcb693a2154a452611b5c265c7412d3101
victomina <victomina@protonmail.com>
2026-07-15 02:39:19 -0300

    Ajustes

--- Agenda_V2/src/main/java/br/org/irede/poo/agenda_v2/Agenda_v2.java
+++ Agenda_V2/src/main/java/br/org/irede/poo/agenda_v2/Agenda_v2.java
@@ -7,41 +7,41 @@ import javax.swing.JOptionPane;
  * @author Victor Veríssimo
  */
 public class Agenda_v2 {
-    
+
     public static void main(String[] args) {
-       boolean continuar = true;
-       
-        while(continuar){
-         String opcao = JOptionPane.showInputDialog(null,
-             "===Agenda==="
-             + "1 - Cadastrar contato \n"
-             + "2 - Listar contatos \n"
-             + "6 - sair \n\n"     
-             + "Escolha uma das opções acima:",
-             "Menu Principal",
-             JOptionPane.PLAIN_MESSAGE
-         );
-         if(opcao == null){
-               break;
-         } 
-         switch(opcao.trim()){
-             case "1" :
-                 JOptionPane.showMessageDialog(null, "Cadastro");
-             case "2" :
-                 JOptionPane.showMessageDialog(null, "Consulta");
-             case "6" :
-                 continuar = false;
-                 break;
-             default:
-                 JOptionPane.showMessageDialog(null, "Opção inválida!! Escolha um numero:(1,2 ou 6)",
-                 "Erro", JOptionPane.ERROR_MESSAGE);
-         }
-       }    
-        JOptionPane.showMessageDialog(null,"Até logo");
+        boolean continuar = true;
+7
+        while (continuar) {
+            String opcao = JOptionPane.showInputDialog(null,
+                    "===Agenda==="
+                    + "1 - Cadastrar contato \n"
+                    + "2 - Listar contatos \n"
+                    + "6 - sair \n\n"
+                    + "Escolha uma das opções acima:",
+                    "Menu Principal",
+                    JOptionPane.PLAIN_MESSAGE
+            );
+            if (opcao == null) {
+                break;
+            }
+            switch (opcao.trim()) {
+                case "1":
+                    JOptionPane.showMessageDialog(null, "Cadastro");
+                case "2":
+                    JOptionPane.showMessageDialog(null, "Consulta");
+                case "6":
+                    continuar = false;
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida!! Escolha um numero:(1,2 ou 6)",
+                            "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Até logo");
     }
-    
- }   
-    /*static int contador = 0;
+
+}
+/*static int contador = 0;
     static Contato[] contatos = new Contato[3];
 
     public static void main(String[] args) {
@@ -85,4 +85,3 @@ public class Agenda_v2 {
         }
         JOptionPane.showMessageDialog(null, mensagem, "Contato", JOptionPane.INFORMATION_MESSAGE);
     }*/
-
