8c5036b048b3c7cf610c7045237b4f26bd9039be
victomina <victomina@protonmail.com>
2026-07-16 02:13:13 -0300

    Adicionando a classe ContatoRepository

--- Agenda2/src/main/java/br/org/irede/poo/agenda2/ContatoRepository.java
+++ Agenda2/src/main/java/br/org/irede/poo/agenda2/ContatoRepository.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.org.irede.poo.agenda2;
+
+import java.io.File;
 
 /**
  *
- * @author Usuário
+ * @author Victor
  */
 public class ContatoRepository {
+    private final String caminhoArquivo;
+
+    public ContatoRepository(String caminhoArquivo) {
+        this.caminhoArquivo = caminhoArquivo;
+        garantirArquivoExiste();
+    }
+
+    private void garantirArquivoExiste() {
+       File arquivo = new File(caminhoArquivo);
+    }
+    
     
 }
