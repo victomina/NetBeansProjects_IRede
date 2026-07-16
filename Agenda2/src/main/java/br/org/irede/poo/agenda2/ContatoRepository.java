bafeed78c53b56aab4e3dbc54cb313558c9cf990
victomina <victomina@protonmail.com>
2026-07-15 22:50:04 -0300

    Adicionando mudanças

---
+++ Agenda2/src/main/java/br/org/irede/poo/agenda2/ContatoRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.org.irede.poo.agenda2;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ContatoRepository {
+    
+}
