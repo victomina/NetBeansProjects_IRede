2bf3de709dceb57495c147e9c24b1a281e8ebc4f
victomina <victomina@gmail.com>
2026-09-21 17:25:05 -0300

    Adicionando a classe com generics

---
+++ CatalogoSemGenerics/src/main/java/br/org/irede/generics/catalogosemgenerics/CatalogoComGenerics.java
@@ -0,0 +1,14 @@
+package br.org.irede.generics.catalogosemgenerics;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Victor Verissimo
+ */
+public class CatalogoComGenerics {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Notebook");
+    }
+}
