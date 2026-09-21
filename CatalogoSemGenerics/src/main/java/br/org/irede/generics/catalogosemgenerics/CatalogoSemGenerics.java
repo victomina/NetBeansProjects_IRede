38f7e7aac0e44fb4dca88a7ff8f5def8e9785e07
victomina <victomina@gmail.com>
2026-09-21 17:14:44 -0300

    Adicionando prodtos para fazer o caasting da lista

--- CatalogoSemGenerics/src/main/java/br/org/irede/generics/catalogosemgenerics/CatalogoSemGenerics.java
+++ CatalogoSemGenerics/src/main/java/br/org/irede/generics/catalogosemgenerics/CatalogoSemGenerics.java
@@ -11,7 +11,13 @@ public class CatalogoSemGenerics {
     public static void main(String[] args) {
         ArrayList lista = new ArrayList<>(); //sem generics
         lista.add("NOtebook");
+        lista.add(2100); //a lista tá aceitando dados do tipo object
+        // mas é precis fazer o casting dos dados
+        String nome = (String) lista.get(0);
+        System.out.println(nome);
         
-        
+        String preco = (String) lista.get(0);
+        System.out.println(preco);
     }
 }
+
